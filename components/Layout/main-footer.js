e28087c2e721367ba507e3ddfa5fdcb5305226ef
Ankur Bardhan <ankur.b@lawsikho.in>
2023-09-13 17:39:16 +0530

    home page is almost made

---
+++ components/Layout/main-footer.js
@@ -0,0 +1,75 @@
+import Link from 'next/link';
+import classes from './main-footer.module.css';
+
+function MainFooter (props) {
+    return (
+    <div class="container">
+        {/* <div class="row">
+        <div class="col-md-12 col-lg-6">
+            <div class="row">
+            <div class="col-lg-8">
+                <img src="https://skillarbitra.ge//assets/frontend/images/logo.svg" class="logo" alt="" />
+                <div class="address_footer">
+                ADDICTIVE LEARNING TECHNOLOGY PRIVATE LIMITED
+                <br/>
+                CIN: U74110DL2017PTC323508
+                <br/>
+                GST: 06AAQCA0212K1ZV
+                <br/>
+                <address>
+                    <strong style="display: block;margin: 8px 0 0px 0;">Gurugram Office</strong>
+                    Corporate Office: 576, Block C, Sushant Lok Phase I,
+                    <br/>
+                    Sector 43, Gurugram, Haryana 122002
+                    <strong style="display: block;margin: 8px 0 0px 0;">Kolkata Office</strong>
+                    Apeejay house, 8th Floor, 15 Park Street,<br> Kolkata 700 016.
+                </address>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <ul>
+                <li><a href="https://skillarbitra.ge/about-us">About Us</a></li>
+                <li><a href="#">Message from CEO</a></li>
+                <li><a href="https://skillarbitra.ge/privacy-policy">Privacy Policy</a></li>
+                </ul>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-12 col-lg-6">
+            <div class="row">
+            <div class="col-lg-4">
+                <ul>
+                <li><a href="#">Help Center</a></li>
+                <li><a href="https://skillarbitra.ge/customer-support">Customer Support</a></li>
+                <li><a href="https://skillarbitra.ge/terms-of-service">Terms of Service</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3">
+                <ul>
+                <li><a href="#">For Clients</a></li>
+                <li><a href="#">For Experts</a></li>
+                <li><a href="https://skillarbitra.ge/refund-policy">Refund policy</a></li>
+                <li><a href="https://skillarbitra.ge/contact-us">Contact us</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-5">
+                <div class="social-sites">
+                <span>Follow us</span>
+                <ul>
+                    <li><a target="_blank" href=" https://instagram.com/skill_arbitrage"><img src="https://skillarbitra.ge//assets/frontend/images/instagram.svg" alt=""></a></li>
+                    <li><a target="_blank" href=" https://twitter.com/SkillArbitrage"><img src="https://skillarbitra.ge//assets/frontend/images/twitter.png" alt=""></a></li>
+                    <li><a target="_blank" href="https://www.youtube.com/@skillarbitrage/featured "><img src="https://skillarbitra.ge//assets/frontend/images/youtube.svg" alt=""></a></li>
+                    <li><a target="_blank" href="https://www.facebook.com/Skill-Arbitrage-100833022847933"><img src="https://skillarbitra.ge//assets/frontend/images/facebook.svg" alt=""></a></li>
+                    <li><a target="_blank" href="https://www.linkedin.com/company/skill-arbitrage"><img src="https://skillarbitra.ge//assets/frontend/images/linkedin.svg" alt=""></a></li>
+                </ul>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div> */}
+    </div>
+
+  )
+}
+
+export default MainFooter;
--- components/home/first-page.js
+++ components/home/first-page.js
@@ -1,26 +1,183 @@
+import Button from '../ui/button';
+import classes from './first-page.module.css';
+
 function FirstPage () {
     
     return (
         <div>
-            <div>
-            <h1>Hire game changing remote workers</h1>
-            <ui>
-                <li>Highly skilled talent too expensive where you are? Hire top global talent at a fraction of that price</li>
-                <li>Increase productivity with flexible remote workers and freelancers. Watch your payroll expenses drop rapidly while upscaling efficiency and output</li>
-                <li>Scale your business faster with less risk, expand your profit margin</li>
-                <li>Give your key people more time off, reduce work pressure on yourself and your main team so you can focus on things that really matter while remote workers take care of everything else</li>
-            </ui>
+            <div className={classes.firstDiv}>
+                <div className={classes.DivFirstChild}>
+                    <h1>Global skill arbitrage is reshaping the world - see how it can change your life forever</h1>
+                </div>
+
+                <div className={classes.DivSecondChild}>
+                    <img src="https://skillarbitra.ge/assets/frontend/images/home-banner.png" />
+                </div>
             </div>
-            
-            <div>
-                <h1>Find awesome remote jobs that pay 5x more</h1>
-                <ui>
-                    <li>Build skills, track record and outreach with us, placing you in the league of top global talent</li>
-                    <li>Increase your income 5x through location-independent freelance work and remote jobs</li>
-                    <li>Work for businesses in US, UK, EU and other advanced economies</li>
-                    <li>Escape toxic workspace culture with lots of pressure and no job satisfaction</li>
-                    <li>Find freedom, say no to the 9-5 routine</li>
-                </ui>
+
+            <div className={classes.normalDiv}>
+                <div className={classes.DivFirstChild}>
+                    <h2>Hire game changing remote workers</h2>
+
+                    <div>
+                        <div>Highly skilled talent too expensive where you are? Hire top global talent at a fraction of that price</div>
+                    </div>
+
+                    <div>
+                        <div>Increase productivity with flexible remote workers and freelancers. Watch your payroll expenses drop rapidly while upscaling efficiency and output</div>
+                    </div>
+
+                    <div>
+                        <div>Scale your business faster with less risk, expand your profit margin</div>
+                    </div>
+
+                    <div>
+                        <div>Give your key people more time off, reduce work pressure on yourself and your main team so you can focus on things that really matter while remote workers take care of everything else</div>
+                    </div>
+
+                    <Button>Sign up here</Button>
+                </div>
+
+                <div className={classes.DivSecondChild}>
+                    <div class="mainImg"><img alt="" src="https://admin.skillarbitra.ge/assets/uploads/media-library/img4-1692698525.png" /></div>
+                </div>
+            </div>
+
+
+            <div className={classes.normalDiv}>
+                <div className={classes.DivFirstChild}>
+                    <div><img alt="" src="https://admin.skillarbitra.ge/assets/uploads/media-library/img2-1692698510.png" /></div>
+                </div>
+
+                <div className={classes.DivSecondChild}>
+                    <h2>Find awesome remote jobs that pay 5x more</h2>
+
+                    <div>
+                        <div>Build skills, track record and outreach with us, placing you in the league of top global talent</div>
+                    </div>
+
+                    <div>
+                        <div>Increase your income 5x through location-independent freelance work and remote jobs</div>
+                    </div>
+
+                    <div>
+                        <div>Work for businesses in US, UK, EU and other advanced economies</div>
+                    </div>
+
+                    <div>
+                    <div>Escape toxic workspace culture with lots of pressure and no job satisfaction</div>
+                    </div>
+
+                    <div>
+                        <div>Find freedom, say no to the 9-5 routine</div>
+                    </div>
+                        <Button>Sign me up</Button>
+                </div>
+            </div>
+
+            <div className={classes.firstDiv}>
+                <div className={classes.blueDivChild}>
+                    <h2>Take a look at this data</h2>
+                    <ul>
+                        <li>As per a KPMG study: Nearly 50% respondents in Asia Pacific are implementing virtual assignments and remote hiring.<br />
+                        In the Americas region, short-term remote work requests are twice as common as in the Asia Pacific region.</li>
+                        <li>Between 2019 and 2021, the number of people primarily working from home tripled from 9 million people to 27.6 million people, according to new 2021 American Community Survey (ACS) 1-year estimates of U.S. Census Bureau.</li>
+                        <li>54.9 per cent of the total employers surveyed said that they anticipate over 50% of their workforce to be remote in the future, as per a study by Deloitte</li>
+                        <li>Upto 47.8 million people in the ASEAN-6 nations (Indonesia, Malaysia, Singapore, Philippines, Thailand, and Vietnam) could shift to working remotely over a multi-year time horizon, Deloitte found in a study</li>
+                        <li>A PwC survey found that 41% of a group of remote workers want to stay fully remote.</li>
+                    </ul>
+                </div>
+
+                <div className={classes.DivSecondChild}>
+                    <div><img alt="" src="https://admin.skillarbitra.ge/assets/uploads/media-library/img2-1692698510.png" /></div>
+                </div>
+            </div>
+
+            <div className={classes.normalDiv}>
+                <div className={classes.DivfirstChild}>
+                    <div class="mainImg"><img alt="" src="https://admin.skillarbitra.ge/assets/uploads/media-library/img4-1692698525.png" /></div>
+                </div>
+
+                <div className={classes.DivSecondChild}>
+                    <h2>SkillArbitrage Manifesto</h2>
+                    <div>
+                        <div>We believe that humanity is at a crossroads, and the best career opportunities in the world will be available to a global talent pool and not be confined to a downtown office, or even limited by national borders.</div>
+                    </div>
+
+                    <div>
+                        <div>We believe that international exports of services will leave exports of goods behind by miles in the years to come.</div>
+                    </div>
+
+                    <div>
+                        <div>We refuse to accept that those living in small towns and developing economies can&rsquo;t have world class careers.</div>
+                    </div>
+
+                    <div>
+                        <div>We refuse to accept that to have a rewarding career you have to compromise on your lifestyle, give up on your freedoms and migrate to large cities.</div>
+                    </div>
+
+                    <div >
+                        <div class="txtWrp"><span class="sbPara">We refuse to accept that you have to spend a fortune and several years of your life in a university to acquire the skills and credentials you need to grow in your career.</span></div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className={classes.midcontainer}>
+                <h2 style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>Success Stories</h2>
+                <strong className={classes.sbHead} style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: 'black'
+                }}>We place the best with the best</strong>
+                
+                <div className={classes.subcontainer}>
+                    
+                        <div className={classes.imgDiv}>
+                            <img alt="" src="https://skillarbitra.ge/assets/uploads/media-library/sonylogo1957-1669024195.png" />
+                        </div>
+                        
+                        <div className={classes.imgDiv}>
+                            <img alt="" src="https://skillarbitra.ge/assets/uploads/media-library/googlelogo-1669024132.png" />
+                        </div>
+                        
+                        <div className={classes.imgDiv}>
+                            <img alt="" src="https://skillarbitra.ge/assets/uploads/media-library/hsbcsymbol-1669024141.png" />
+                        </div>
+                        
+                        <div className={classes.imgDiv}>
+                            <img alt="" src="https://skillarbitra.ge/assets/uploads/media-library/lt-1669024157.png" />
+                        </div>
+
+                        <div className={classes.imgDiv}>
+                            <img alt="" src="https://skillarbitra.ge/assets/uploads/media-library/logotruecolorsoriginal-1669024171.png" />
+                        </div>
+
+                        <div className={classes.imgDiv}>
+                            <img alt="" src="https://skillarbitra.ge/assets/uploads/media-library/pwc-1669024879.png" />
+                        </div>
+
+                        <div className={classes.imgDiv}>
+                            <img alt="" src="https://skillarbitra.ge/assets/uploads/media-library/kpmg-1669024948.png" />
+                        </div>
+
+                        <div className={classes.imgDiv}>
+                            <img alt="" src="https://skillarbitra.ge/assets/uploads/media-library/deloittelogo-1669024097.png" />
+                        </div>
+                        <div className={classes.imgDiv}>
+                            <img alt="" src="https://skillarbitra.ge/assets/uploads/media-library/capgeminilogo-1669024086.png" />
+                        </div>
+                        <div className={classes.imgDiv}>
+                            <img alt="" src="https://skillarbitra.ge/assets/uploads/media-library/zohologo512px-1669024206.png" />
+                        </div>
+                                
+                </div>
+                
             </div>
         </div>
     );
